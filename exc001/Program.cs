a2e23290fa9724758903f8234d98f92731a167cb
Andrew <vishnyaishere@gmail.com>
2022-08-23 12:54:32 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -3,3 +3,21 @@
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
 // в которой находится эта точка.
 
+Console.WriteLine("Input x coord: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input y coord: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+if (xa > 0 && ya > 0){
+    Console.WriteLine("Quarter I");
+}
+else if (xa < 0 && ya > 0){
+    Console.WriteLine("Quarter II");
+}
+else if (xa < 0 && ya < 0){
+    Console.WriteLine("Quarter III");
+}
+else if (xa > 0 && ya < 0){
+    Console.WriteLine("Quarter IV");
+}
+else Console.WriteLine("One/both of the coords is/are not on any of the quarters");
